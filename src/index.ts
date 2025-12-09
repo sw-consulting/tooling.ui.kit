358fc3084d7a6755c36887137d47c180328caf21
Maxim Samsonov <m.samsonov@computer.org>
2025-12-10 00:46:23 +0300

    chore: version bump to 1.2.1

--- src/index.ts
+++ src/index.ts
@@ -24,4 +24,4 @@
 export { default as ActionButton } from './components/ActionButton.vue'
 export { default as ActionButton2L } from './components/ActionButton2L.vue'
 
-export const version = '1.0.0'
+export const version = '1.2.1'
